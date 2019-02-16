9b69487d1ce4e4acad522f235e598962b101c1bd
pijuskri <pijuskri@gmail.com>
2019-02-16 18:52:14 +0200

    basic stuff

--- Take a Chance/Assets/Card.cs
+++ Take a Chance/Assets/Card.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class CardInfoImport
+{
+    public List<CardInfo> cards;
+}
+[System.Serializable]
+public class CardInfo
+{
+    public string name;
+}
 public class Card : MonoBehaviour
 {
     // Start is called before the first frame update
     bool isPickedUp = false;
+    public int player = -1;
+    public GameObject cardPicture;
     void Start()
     {
         
@@ -14,16 +26,17 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (isPickedUp)
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.y = 0;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.99f));
             transform.position = mousePos;
-            Debug.Log(Input.mousePosition);
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
         }
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyUp(KeyCode.Mouse0) && isPickedUp)
         {
             isPickedUp = false;
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.3f, transform.position.z);
         }
     }
     void OnMouseOver()
---
+++ Take a Chance/Assets/Gamelogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Player
+{
+    public string name { get; set; }
+    public int health { get; set; }
+    public int mana { get; set; }
+    public List<Card> cards { get; set; }
+    public Player(string name)
+    {
+        this.name = name;
+        health = 10;
+        mana = 0;
+        cards = new List<Card>();
+    }
+}
+public class Gamelogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Player player1;
+    public Player player2;
+    public List<CardInfo> cardInfos;
+    void Start()
+    {
+        player1 = new Player("player1");
+        player2 = new Player("player2");
+        ReadCardInfo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ReadCardInfo()
+    {
+        cardInfos = new List<CardInfo>();
+        CardInfoImport temp = JsonUtility.FromJson<CardInfoImport>(File.ReadAllText(Application.dataPath + "/cards.json"));
+        cardInfos = temp.cards;
+    }
+}
